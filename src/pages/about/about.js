5637075027c88e91026cad11c0a83dc81fd9f3e9
fu1252 <fu1252@163.com>
2019-11-17 20:44:11 +0800

    完成侧边栏导航

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import { useHistory,useLocation} from "react-router-dom";
-import { useStoreState, useStoreActions, action } from "easy-peasy";
+import { useHistory, useLocation } from 'react-router-dom';
+import { useStoreState, useStoreActions, action } from 'easy-peasy';
 
 
-function About(){
-  let history=useHistory()
-  const role = useStoreState(state => state.user.userRole);
-  const changeRole=useStoreActions(actions=>actions.user.changeRole)
+function About() {
+  let history = useHistory()
+  const role = useStoreState(state => state.user.userRole)
+  const changeRole = useStoreActions(actions => actions.user.changeRole)
 
-if(role==='user'){return(
-  <h1>您没有权限访问这个页面</h1>
-)}
+  if (role === 'user') {
+    return (
+      <h1>您没有权限访问这个页面</h1>
+    )
+  }
   return (
-   <div>
-          <h2>我是about组件</h2>
-          <div>
-          <button className='custom-btn' onClick={()=>changeRole('user')}>切换权限</button>
-          </div>
-          <button className='custom-btn' onClick={()=>history.push('/user')}>个人中心</button>
-          <button className='custom-btn' onClick={()=>history.push('/login')}>登录</button>
-   </div>
+    <div>
+      <h2>我是about组件</h2>
+      <div>
+        <button className='custom-btn' onClick={() => changeRole('user')}>切换权限</button>
+      </div>
+      <button className='custom-btn' onClick={() => history.push('/user')}>个人中心</button>
+      <button className='custom-btn' onClick={() => history.push('/login')}>登录</button>
+    </div>
   )
 }
 
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,102 +1,130 @@
-import React, { lazy, useEffect, useState } from "react";
-import { Drawer, NavBar, Icon } from "antd-mobile";
-import { Switch, Route, Redirect, useHistory, NavLink } from "react-router-dom";
-import http from "@/http/http";
-import { getLocalStorage, getSessionStorage, setSessionStorage } from "@/utils/storage";
-import { useStoreState, useStoreActions } from "easy-peasy";
-import style from "./home.less";
-import classnames from "classnames";
-import ReactSVG from "react-svg";
+import React, { lazy, useEffect, useState } from 'react'
+import { Drawer, NavBar, Icon } from 'antd-mobile'
+import { Switch, Route, Redirect, useHistory,useLocation } from 'react-router-dom'
+import http from '@/http/http'
+import { getLocalStorage, getSessionStorage, setSessionStorage } from '@/utils/storage'
+import { useStoreState, useStoreActions } from 'easy-peasy'
+import style from './home.less'
+import classnames from 'classnames'
+import ReactSVG from 'react-svg'
 
-const About = lazy(() => import("@/pages/about/about"));
-const User = lazy(() => import("@/pages/user/user"));
-const Main = lazy(() => import("./main"));
+const About = lazy(() => import('@/pages/about/about'))
+const User = lazy(() => import('@/pages/user/user'))
+const Main = lazy(() => import('./main'))
 
 function Home() {
-  let history = useHistory();
-  const isOpenSidebar = useStoreState(state => state.layout.isOpenSidebar);
-  const role = useStoreState(state => state.user.userRole);
-  const toggleSidebar = useStoreActions(actions => actions.layout.toggleSidebar);
-  const [currentClickNav, setCurrentClickNav] = useState({show:false,member:false});
-  console.log("TCL: Home -> currentClickNav", currentClickNav);
+  let history = useHistory()
+  let location=useLocation()
+  const isOpenSidebar = useStoreState(state => state.layout.isOpenSidebar)
+  const role = useStoreState(state => state.user.userRole)
+  const toggleSidebar = useStoreActions(actions => actions.layout.toggleSidebar)
+  const [currentClickNav, setCurrentClickNav] = useState({})
+  console.log('TCL: Home -> currentClickNav', currentClickNav)
 
   const baseNavList = [
-    { text: "销售数据", role: ["user", "admin"], icon: "saleTab", path: "sale" },
-    { text: "设备管理", role: ["user", "admin"], icon: "machine", path: "machine" },
-    { text: "商品管理", role: ["user", "admin"], icon: "shop", path:'abc',
-    children: [
-      { text: "礼物", role: ["admin"], icon: "order", path: "abc" },
-      { text: "打折", role: ["admin"], icon: "account", path: "bbc" }
-    ] },
-    { text: "订单管理", role: ["admin"], icon: "order", path: "order" },
-    { text: "账目管理", role: ["admin"], icon: "account", path: "account" },
+    { text: '销售数据', roles: ['user', 'admin'], icon: 'saleTab', path: '/sale/fdd' },
+    { text: '设备管理', roles: ['user', 'admin'], icon: 'machine', path: '/machine' },
     {
-      text: "会员管理",
-      role: ["admin"],
-      icon: "member",
-      path: "gift",
+      text: '商品管理', roles: ['user', 'admin'], icon: 'shop',key:'shop',
       children: [
-        { text: "礼物", role: ["admin"], icon: "order", path: "gift" },
-        { text: "打折", role: ["admin"], icon: "account", path: "profit" }
+        { text: '地方', roles: ['user','admin'], icon: 'order', path: '/abc' },
+        { text: '哈佛', roles: ['admin'], icon: 'account', path: '/bbc' }
+      ]
+    },
+    { text: '订单管理', roles: ['admin'], icon: 'order', path: '/order' },
+    { text: '账目管理', roles: ['admin'], icon: 'account', path: '/account' },
+    {
+      text: '会员管理',
+      roles: ['user','admin'],
+      icon: 'member',
+      key:'member',
+      children: [
+        { text: '礼物', roles: ['admin'], icon: 'order', path: '/gift' },
+        { text: '打折', roles: ['user','admin'], icon: 'account',key:'subAccount',  children: [
+          { text: '家里的', roles: ['user','admin'], icon: 'order', path: '/tttt' },
+          { text: '电风扇', roles: ['admin'], icon: 'account', path: '/ggg',key:'subsub',children:[
+            { text: '大幅度', roles: ['admin'], icon: 'order', path: '/445' },
+
+          ] }
+        ]}
       ]
     }
-  ];
-
-  const navList = baseNavList.filter(item => item.role.includes(role));
+  ]
+  
+  // 过滤路由权限
+  const cloneData=JSON.parse(JSON.stringify(baseNavList))
+  function filterData(data){
+    for (let index = 0; index < data.length; index++) {
+      const ele = data[index]
+      if(!ele.roles.includes(role)){
+        data.splice(index,1)
+        index--
+      }else if(ele.children){
+        filterData(ele.children)
+      }
+     
+    }
+  }
+  filterData(cloneData)
+  const navList = cloneData
 
   useEffect(() => {
     async function getData() {
-      const objectId = getLocalStorage("userData").object_id;
-      const userInfoData = getSessionStorage("userInfo");
+      const objectId = getLocalStorage('userData').object_id
+      const userInfoData = getSessionStorage('userInfo')
       if (userInfoData) {
-        return;
+        return
       } else {
-        const res = await http.get(`operators/${objectId}`);
-        setSessionStorage("userInfo", res);
+        const res = await http.get(`operators/${objectId}`)
+        setSessionStorage('userInfo', res)
       }
     }
-    getData();
-  }, []);
+    getData()
+  }, [])
+
+  function onListClick(item){
+    if(item.children){
+      setCurrentClickNav({ ...currentClickNav, [item.key]: !currentClickNav[item.key] })
+    }else{
+      history.push(item.path)
+    }
+  }
+
+  function listEle(data) {
+    return (
+      data.map(item => (
+        <div key={item.text} >
+          <li onClick={() =>onListClick(item) }>
+            <div className={classnames({listItem:true,navActive:location.pathname==item.path})}>
+              <ReactSVG className="inlineSVG" src={require(`@/assets/${item.icon}.svg`)} />
+              <span className="text">{item.text}</span>
+              {item.children && <ReactSVG  className={classnames({inlineSVG:true,
+                'icon-open':currentClickNav[item.key]
+              })} src={require('@/assets/right.svg')} />}
+            </div>
+          </li>
+          {item.children &&<div className={classnames({
+            'subList-hidden':!currentClickNav[item.key]
+          })}> {listEle(item.children)}</div>}
+        </div>
+      ))
+    )
+  }
 
   function sidebar() {
     return (
       <ul className={style.sidebarList}>
-        <li className="username" onClick={() => history.push("/user")}>
-          {" "}
+        <li className="username" onClick={() => history.push('/user')}>
+          {' '}
           <span className="avatar"></span> test_free
         </li>
-
-        {navList.map(item => (
-          <div key={item.text}>
-            <NavLink exact  activeClassName="navActive" to={item.path}>
-              <li  onClick={() => setCurrentClickNav({...currentClickNav,[item.path]:!currentClickNav[item.path]})}>
-                <div className="listItem">
-                  <ReactSVG className="inlineSVG" src={require(`@/assets/${item.icon}.svg`)} />
-                  <span className="text">{item.text}</span>
-                  {item.children && <ReactSVG className="inlineSVG" src={require(`@/assets/right.svg`)} />}
-                </div>
-              </li>
-            </NavLink>
-            {item.children &&<div className={classnames({'subList-hidden':currentClickNav[item.path]})}>
-              {item.children.map(subitem => (
-                <NavLink key={subitem.text} exact activeClassName="navActive" to={subitem.path}>
-                  <li>
-                    <div className="listItem">
-                      <ReactSVG className="inlineSVG" src={require(`@/assets/${subitem.icon}.svg`)} />
-                      <span className="text">{subitem.text}</span>
-                    </div>
-                    {subitem.children && <div className="subList">我是子元素</div>}
-                  </li>
-                </NavLink>
-              ))}</div>}
-          </div>
-        ))}
+        {listEle(navList)}
       </ul>
-    );
+    )
   }
 
   function NotFound() {
-    return <div className={style.notfound}>来到没有页面的沙漠地带</div>;
+    return <div className={style.notfound}>来到没有页面的沙漠地带</div>
   }
 
   return (
@@ -122,7 +150,7 @@ function Home() {
         </Switch>
       </Drawer>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
